66d866f478053b7835e4c54bfe598e8c295fe0f6
VIKRANT SINGH <VIKRANTSINGH.VS53@GMAIL.COM>
2024-04-04 23:34:30 +0530

    Added DSA WorkSpace

---
+++ springBoot/src/main/java/com/learning/springBoot/Application.java
@@ -0,0 +1,15 @@
+package com.learning.springBoot;
+
+import ch.qos.logback.core.net.SyslogOutputStream;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hellow World");
+	}
+
+}
