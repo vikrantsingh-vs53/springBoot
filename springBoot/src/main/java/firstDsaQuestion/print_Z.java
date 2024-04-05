595145309038097de9f3c8a01ae2abf21145c5a8
VIKRANT SINGH <VIKRANTSINGH.VS53@GMAIL.COM>
2024-04-05 23:40:48 +0530

    Create print_Z.java

---
+++ springBoot/src/main/java/firstDsaQuestion/print_Z.java
@@ -0,0 +1,19 @@
+package firstDsaQuestion;
+
+
+
+/*Question Statement:
+        1. We have to print a "Z" consisting of 5 rows.*/
+public class print_Z {
+
+    static void print_Z() {
+        System.out.println("* * * * *");
+        System.out.println("      * ");
+        System.out.println("    * ");
+        System.out.println("  * ");
+        System.out.println("* * * * *");
+    }
+    public static void main(String[] args) {
+        print_Z();
+    }
+}
